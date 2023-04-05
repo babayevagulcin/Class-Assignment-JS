0b81da2aebce9046485ab1a17862eb2acd510d5d
[babayevagulcin] <[7ebv570@code.edu.az]>
2023-04-05 22:08:28 +0400

    js-file added

---
+++ 19.JS-let/script.js
@@ -0,0 +1,27 @@
+console.log(4 + 2 + "5" + 2 + "a");
+
+let multiply = 12 * 8;
+console.log(multiply);
+
+let x = 10 / 2;
+console.log(x);
+
+let num1 = 20;
+let num2 = 17;
+console.log(num1+num2);
+
+// let name='Gulchin',surname='Babayeva',year='2002'
+// let info=name+" "+surname+" "+year
+// console.log(info);
+
+let remainder=(17%12)
+console.log(remainder);
+
+let cityName="camelCase"
+cityName="Baku"
+cityName="Gence"
+console.log(cityName);
+
+let obj1={name:"Gulchin",surname:"Babayeva",age:"20"}
+let obj2={name:"Gulchin",surname:"Babayeva",age:"20"}
+console.log(obj2);
