1c42ba20b2799447685aa739bbc5f2a96dca6227
Babayeva13 <126855850+babayevagulcin@users.noreply.github.com>
2023-04-10 00:51:20 -0700

    Add files via upload

---
+++ 20.java/main.js
@@ -0,0 +1,15 @@
+let text = "there";
+
+console.log(`hello ${3 + 8}`); //hello 11
+console.log(`hello ${"3+8"}`); //hello 3+8
+console.log(`hello ${"text"}`); //hello text
+console.log(`hello ${text}`); //hello there
+
+let firstName = "Gulchin";
+let lastName = "Babayeva";
+let age = 20;
+let city = "Ganja";
+let uni = "GDU";
+
+let info = `${firstName} ${lastName} ${age} ${city} ${uni}`
+console.log(info);
