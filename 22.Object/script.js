8ceb80daccba20a7eb956b07a8f0ae97e08c264c
Babayeva13 <126855850+babayevagulcin@users.noreply.github.com>
2023-04-10 00:51:02 -0700

    Add files via upload

---
+++ 22.Object/script.js
@@ -0,0 +1,28 @@
+//8
+const obj ={
+    id: 3,
+    firstName:"Gulchin",
+    lastName:"Babayeva",
+    location: "GDU 2",
+}
+console.log(obj);
+
+//9
+const obj2 ={
+    id: 1,
+    firstName:"Gulchin",
+    lastName:"Babayeva",
+    location: "GDU 2",
+}
+console.log("obj",obj);
+
+//13
+const obj3 ={
+    id: 3,
+    firstName:"Gulchin",
+    lastName:"Babayeva",
+    location: "GDU 2",
+}
+
+delete(obj3.location)
+console.log(obj3);
