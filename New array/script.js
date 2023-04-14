fc23e8a2a03a3c4741361b8019e5f87ffb3df6a2
Babayeva13 <126855850+babayevagulcin@users.noreply.github.com>
2023-04-14 17:40:18 +0400

    Add files via upload

---
+++ New array/script.js
@@ -0,0 +1,26 @@
+// /////// 6.Filter Method
+// const mixArray=['1',2,4,'a',false,'9',12,undefined,3,true,null]
+// // 6.1
+// const newMixarr=mixArray.filter((item)=>typeof item==='number')
+// console.log(newMixarr);
+
+// // 6.2
+// const newMixarr1=mixArray.filter((item)=>typeof item==='string')
+// console.log(newMixarr1);
+
+// // 6.3
+// const newMixarr2=mixArray.filter((item)=>typeof item==='boolean')
+// console.log(newMixarr2);
+
+// // 6.4
+// const newMixarr3=mixArray.filter(item => !item)
+// console.log(newMixarr3); 
+
+// /////// 7.Filter and Map Methods
+// // convert to "number" each value of "arr7" and find max value in "arr7"
+const arr7=[44,3,6,'2',6,true,undefined,'55',null]
+
+// 7.1
+const newArr9=arr7.map(item=>Number(item))
+const newArr10=Math.max(...newArr9.filter((item)=> item)) 
+console.log(newArr10);
