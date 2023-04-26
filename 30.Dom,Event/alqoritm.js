e14ed24200f2a60f401f5247e4d892a9a4d36e44
[babayevagulcin] <[7ebv570@code.edu.az]>
2023-04-26 22:20:04 +0400

    Dom

---
+++ 30.Dom,Event/alqoritm.js
@@ -0,0 +1,5 @@
+//8. Write a function that takes an array of numbers as input and returns the median of the array 
+// (i.e., the middle number when the array is sorted).
+
+//11 Create a function that takes a string and returns an object with properties representing the frequency of each character in the string.
+//  For example, if the string contains "hello world", the function should return an object with properties "h", "e", "l", "o", "w", "r", and "d" and values 1, 1, 3, 2, 1, 1, and 1 respectively, indicating that each character occurs once or more in the string.
